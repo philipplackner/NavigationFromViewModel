16a82e1ca5be037039989465c230d0de7129ff2b
Philipp Lackner <mail@pl-coding.com>
2024-09-15 15:06:12 +0200

    Initial commit

---
+++ app/src/main/java/com/plcoding/navigationfromviewmodel/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.plcoding.navigationfromviewmodel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.navigationfromviewmodel.ui.theme.NavigationFromViewModelTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigationFromViewModelTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    
+                }
+            }
+        }
+    }
+}
